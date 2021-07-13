5c0a1ef90275fb5757477bfb8c979d2ccbd1fd98
h-kurtz <hckurtz@ucsc.edu>
2021-07-13 12:36:40 -0700

    Rotate to player velocity, clamped magnitues, refactoring

--- GJLGameJam2021Project/Assets/Scripts/Movement.cs
+++ GJLGameJam2021Project/Assets/Scripts/Movement.cs
@@ -5,34 +5,55 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // variables
-    private Rigidbody2D RB;
+    private Rigidbody2D rb;
     public float acceleration = 0.05f;
+    public float maxMoveSpeed = 1f;
+    public float rotSpeed = 2.5f;
         
     // Start is called before the first frame update
     void Start()
     {
-        RB = GetComponent<Rigidbody2D>();
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        rb.velocity += GetInputAcceleration();
+
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxMoveSpeed);
+
+        transform.rotation = RotateToDirection(rb.velocity);
+    }
+
+    private Quaternion RotateToDirection(Vector2 direction)
+    {
+        Quaternion rot = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.up, direction));
+        return Quaternion.Slerp(transform.rotation, rot, rotSpeed * Time.deltaTime);
+    }
+
+    private Vector2 GetInputAcceleration()
+    {
+        Vector2 accelVec = new Vector2(0, 0);
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            RB.velocity += new Vector2(acceleration, 0);
+            accelVec.x += acceleration;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            RB.velocity += new Vector2(-acceleration, 0);
+            accelVec.x -= acceleration;
         }
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            RB.velocity += new Vector2(0, acceleration);
+            accelVec.y += acceleration;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            RB.velocity += new Vector2(0, -acceleration);
+            accelVec.y -= acceleration;
         }
+
+        return Vector2.ClampMagnitude(accelVec, acceleration);
     }
 }
