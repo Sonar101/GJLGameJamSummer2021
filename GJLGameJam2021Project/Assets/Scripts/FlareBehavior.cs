955e9c5dc4cfc9ce556385ab0bc1c5ad3268d1ee
h-kurtz <hckurtz@ucsc.edu>
2021-07-14 09:04:39 -0700

    flares fade out and destroy themselves

---
+++ GJLGameJam2021Project/Assets/Scripts/FlareBehavior.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class FlareBehavior : MonoBehaviour
+{
+    public float lifetime = 5f;
+    public float fadetime = 1f;
+
+    ParticleSystem psFlare;
+    ParticleSystem psSmoke;
+    Light2D lightFlare;
+
+    float lightIntensity;
+    bool fade = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        psFlare = GetComponent<ParticleSystem>();
+        psSmoke = GetComponentInChildren<ParticleSystem>();
+        lightFlare = GetComponent<Light2D>();
+
+        lightIntensity = lightFlare.intensity;
+
+        StartCoroutine(FadeOut(lifetime, fadetime));
+        Destroy(gameObject, lifetime);
+    }
+
+    private void Update()
+    {
+        if (fade)
+        {
+            lightFlare.intensity -= Time.deltaTime * lightIntensity / fadetime;
+        }
+    }
+
+    IEnumerator FadeOut (float lifeT, float fadeT)
+    {
+        yield return new WaitForSeconds(lifeT - fadeT);
+        fade = true;
+    }
+}
