05828caab45a05b143e054611f36dcc97242078c
h-kurtz <hckurtz@ucsc.edu>
2021-07-16 09:18:47 -0700

    doorway functionality and button triggers

--- GJLGameJam2021Project/Assets/Scripts/DoorController.cs
+++ GJLGameJam2021Project/Assets/Scripts/DoorController.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorController : MonoBehaviour
+public class DoorController : Interactable
 {
     public float moveDuration = 1;
     public float rotationOffset = 90f;
+
+    public int doorID = -1;
 
     Vector3 closedRotation;
     Vector3 openRotation;
@@ -15,21 +17,20 @@ public class DoorController : MonoBehaviour
     float progress = 0;
 
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
         closedRotation = transform.rotation.eulerAngles;
         openRotation = closedRotation;
         openRotation.z += rotationOffset;
+
+        base.Start();
+        LevelManager.current.onButtonPress += ButtonOpen;
+        LevelManager.current.onCloseAllDoors += Close;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            ToggleOpen();
-        }
-
         if ((open && progress <= 1) || (!open && progress >= 0))
         {
             progress += Time.deltaTime / moveDuration * (open ? 1 : -1);
@@ -37,15 +38,36 @@ public class DoorController : MonoBehaviour
         }
     }
 
-    void ToggleOpen()
+    void ToggleState()
     {
         open = !open;
     }
+
+    protected override void Interact()
+    {
+        if (base.inRange)
+            ToggleState();
+    }
+
+    void ButtonOpen(int id)
+    {
+        if (doorID == id)
+            open = true;
+    }
+
+    void Close() { open = false; }
 
     private void LerpRotation(float progress)
     {
         transform.rotation = Quaternion.Slerp(Quaternion.Euler(closedRotation),
                                               Quaternion.Euler(openRotation),
                                               progress);
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        LevelManager.current.onButtonPress -= ButtonOpen;
+        LevelManager.current.onCloseAllDoors -= Close;
     }
 }
--- GJLGameJam2021Project/Assets/Scripts/LevelManager.cs
+++ GJLGameJam2021Project/Assets/Scripts/LevelManager.cs
@@ -5,5 +5,31 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    //public event 
+    public static LevelManager current;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public event Action<int> onButtonPress;
+    public void ButtonPress(int id)
+    {
+        if (onButtonPress != null)
+            onButtonPress(id);
+    }
+
+    public event Action onCloseAllDoors;
+    public void CloseAllDoors()
+    {
+        if (onCloseAllDoors != null)
+            onCloseAllDoors();
+    }
+
+    public event Action onInteract;
+    public void Interact()
+    {
+        if (onInteract != null)
+            onInteract();
+    }
 }
