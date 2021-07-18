08e140ccd059e48de4446283eaaeb8217f4317ff
Nathann Latimore <nlatimor@ucsc.edu>
2021-07-18 00:10:34 -0700

    Added invisible walls to level scene

---
+++ GJLGameJam2021Project/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [TextArea(3, 10)]
+    public string[] ceoLogs;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
