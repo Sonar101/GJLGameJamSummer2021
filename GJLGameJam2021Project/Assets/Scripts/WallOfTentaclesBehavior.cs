5393ab0b41fb40e600836af0a64f1b1ce97f99a5
KapeRay <67844957+KapeRay@users.noreply.github.com>
2021-07-22 23:08:33 -0700

    Updated WOT to hopefully be better with its movement

--- GJLGameJam2021Project/Assets/Scripts/WallOfTentaclesBehavior.cs
+++ GJLGameJam2021Project/Assets/Scripts/WallOfTentaclesBehavior.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WallOfTentaclesBehavior : MonoBehaviour
 {
     public Transform Player;
-    public float maxDistance = 1;
+    public float maxHeight = -15f;
     public float slowSpeed = 0.001f;
     public float finalStretchSpeed = 0.1f;
     // Start is called before the first frame update
@@ -17,6 +17,7 @@ public class WallOfTentaclesBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector3 position = transform.position;
         
         position.y += slowSpeed;
@@ -25,6 +26,19 @@ public class WallOfTentaclesBehavior : MonoBehaviour
         if (Player.position.y >= -15f)
         {
             position.y += finalStretchSpeed;
+            transform.position = position;
+        }
+        */
+        Vector3 position = transform.position;
+        if (Player.transform.position.y >= maxHeight)
+        {
+            position += new Vector3(0, finalStretchSpeed * Time.deltaTime, 0);
+            transform.position = position;
+            
+        }
+        else
+        {
+            position += new Vector3(0, slowSpeed * Time.deltaTime, 0);
             transform.position = position;
         }
     }
