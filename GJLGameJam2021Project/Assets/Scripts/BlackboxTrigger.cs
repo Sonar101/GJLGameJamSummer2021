de17be6a4a13f22a9408db3d3c03953c356ce593
KapeRay <67844957+KapeRay@users.noreply.github.com>
2021-07-22 19:55:42 -0700

    Black box takes a couple of E presses to destroy it

--- GJLGameJam2021Project/Assets/Scripts/BlackboxTrigger.cs
+++ GJLGameJam2021Project/Assets/Scripts/BlackboxTrigger.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class BlackboxTrigger : Interactable
 {
-
+    private SpriteRenderer destroying;
+    public Sprite part1Destroy;
+    public Sprite part2Destroy;
+    public Sprite part3Destroy;
+    private int keyPressCounter = 0;
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+        destroying = GetComponent<SpriteRenderer>();
     }
 
     protected override void Interact()
     {
-        LevelManager.current.DestroyBlackBox();
-        Destroy(gameObject);
+        if (Input.GetKeyDown(KeyCode.E) && keyPressCounter == 0)
+        {
+            destroying.sprite = part1Destroy;
+            keyPressCounter = 1;
+            print(keyPressCounter);
+        }
+        else if(Input.GetKeyDown(KeyCode.E) && keyPressCounter == 1)
+        {
+            destroying.sprite = part2Destroy;
+            keyPressCounter = 2;
+            print(keyPressCounter);
+        }
+        else if(Input.GetKeyDown(KeyCode.E) && keyPressCounter == 2)
+        {
+            destroying.sprite = part3Destroy;
+            keyPressCounter = 3;
+            print(keyPressCounter);
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && keyPressCounter == 3)
+        {
+            LevelManager.current.DestroyBlackBox();
+            Destroy(gameObject);
+            //keyPressCounter = 0;
+        }
+        
+        
     }
 
     protected override void OnDestroy()
