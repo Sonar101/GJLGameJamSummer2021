836851eb575a6cbab64d18159812dbf501b9fdf8
KapeRay <67844957+KapeRay@users.noreply.github.com>
2021-07-25 00:53:13 -0700

    Added Screenshake at random intervals

---
+++ GJLGameJam2021Project/Assets/Scripts/ScreenShakeController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakeController : MonoBehaviour
+{
+    private float shakeTimeRemaining, shakePower;
+    private bool isScreenShaking = false;
+    public AudioSource roarSFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isScreenShaking)
+        {
+            
+            int rand = Random.Range(0, 2000);
+            if (LevelManager.current.GetBlackBoxBroken())
+            {
+
+                print(rand);
+                if (rand == 17)
+                {
+                    StartShake(.5f, 0.3f);
+                    //roarSFX?.Play();
+                    StartCoroutine(checkingTime());
+                }
+
+
+            }
+            
+        }
+       
+        
+        
+        
+        /*
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartShake(.5f, 1f);
+        }
+        */
+    }
+    private void LateUpdate()
+    {
+       if(shakeTimeRemaining > 0)
+        {
+            shakeTimeRemaining -= Time.deltaTime;
+            float xAmount = Random.Range(-1f, 1f) * shakePower;
+            float yAmount = Random.Range(-1f, 1f) * shakePower;
+
+            transform.position += new Vector3(xAmount, yAmount, 0);
+        }
+    }
+    public void StartShake(float length, float power)
+    {
+        shakeTimeRemaining = length;
+        shakePower = power;
+    }
+    private IEnumerator checkingTime()
+    {
+        isScreenShaking = true;
+        // process pre-yield
+        yield return new WaitForSeconds(20.0f);
+        // process post-yield
+        isScreenShaking = false;
+    }
+}
